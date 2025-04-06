2a90fac842e3b615604ab5ad2a871fcde96991c9
Рустам Разабеков <rustamrazabekov1705@gmail.com>
2025-04-06 16:20:41 +0600

    init: initialized project

---
+++ src/main/java/com/rustam/sneaker_store/SneakerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.sneaker_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerStoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SneakerStoreApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rustam/sneaker_store/SneakerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustam.sneaker_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerStoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
